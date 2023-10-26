0aa434be24533c8d26d5cff8e23525990e4e6862
DaemonWhite <matheo.daemon@gmail.com>
2023-10-26 16:30:02 +0200

    add keys controller

--- billy_engine/src/eventkeyboard/mod.rs
+++ billy_engine/src/eventkeyboard/mod.rs
@@ -1,8 +1,10 @@
 pub mod eventkeyboard {
 	use std::{io, time::Duration};
+	use std::sync::{Arc, Mutex};
 	use lazy_static::lazy_static;
 	use crossterm::event::{Event,read, poll, KeyCode, KeyEvent};
 	use crate::event::{create, publish, subscribe};
+
 
 	const KEYS_UP: &str = "UP";
 	const KEYS_DOWN: &str = "DOWN";
@@ -26,6 +28,77 @@ pub mod eventkeyboard {
 
 	static mut LISTEN_KEYBOARD: bool= true;
 
+	pub trait ControllerUi {
+		fn key_up(&mut self);
+		fn key_down(&mut self);
+		fn key_right(&mut self);
+		fn key_left(&mut self);
+		fn key_enter(&mut self);
+		fn key_esc(&mut self);
+		fn focus(&mut self);
+	}
+
+	pub fn link_event_keyboard<C>(c: Arc<Mutex<C>>)
+	where
+		C: ControllerUi + Send +'static,
+	{
+		let up = Arc::clone(&c);
+		let down = Arc::clone(&c);
+		let right = Arc::clone(&c);
+		let left = Arc::clone(&c);
+		let enter = Arc::clone(&c);
+		let esc = Arc::clone(&c);
+		let focus = Arc::clone(&c);
+
+		subscribe(KEYS_UP,
+			move || {
+				let mut  c = up.lock().unwrap();
+				c.key_up();
+			}
+		);
+
+		subscribe(KEYS_DOWN,
+			move || {
+				let mut  c = down.lock().unwrap();
+				c.key_down();
+			}
+		);
+
+		subscribe(KEYS_LEFT,
+			move || {
+				let mut  c = left.lock().unwrap();
+				c.key_left();
+			}
+		);
+
+		subscribe(KEYS_RIGHT,
+			move || {
+				let mut  c = right.lock().unwrap();
+				c.key_right();
+			}
+		);
+
+				subscribe(KEYS_ENTER,
+			move || {
+				let mut  c = enter.lock().unwrap();
+				c.key_enter();
+			}
+		);
+
+		subscribe(KEYS_ESC,
+			move || {
+				let mut  c = focus.lock().unwrap();
+				c.key_esc();
+			}
+		);
+
+		subscribe(KEYS_FOCUS,
+			move || {
+				let mut  c = esc.lock().unwrap();
+				c.focus();
+			}
+		);
+	}
 	pub fn end_keyboard() {
 		unsafe {
 			LISTEN_KEYBOARD = false;
@@ -40,6 +113,7 @@ pub mod eventkeyboard {
 
 	pub fn init_event_keyboard() {
 		let rk = &REGISTERY_KEYS;
+		subscribe("GAME_END", end_keyboard);
 		for _name in rk.iter() {
 			create(_name);
 		}
@@ -66,24 +140,10 @@ pub mod eventkeyboard {
 				if poll(Duration::from_millis(REFRESH_KEYS))? {
 					match read()? {
 						Event::FocusGained => publish(KEYS_FOCUS),
-						// Event::Key(event) => println!("{:?}\r", event),
 						Event::Key(event) => registery_keys(event),
-						Event::Resize(_w, _h) => break,
 						_ => println!("no match\r"),
 					}
 				}
-			}
-		}
-		Ok(())
-	}
-
-	pub fn print_events() -> io::Result<()> {
-		loop {
-			match read()? {
-				Event::FocusGained => println!("Enter\r"),
-				Event::Key(event) => println!("{:?}\r", event),
-				Event::Resize(_w, _h) => break,
-				_ => println!("no match\r"),
 			}
 		}
 		Ok(())
