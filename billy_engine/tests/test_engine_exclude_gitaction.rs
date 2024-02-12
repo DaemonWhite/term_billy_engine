08201d94f96955ecf73b8c1840e4e495c327c3b0
DaemonWhite <matheo.daemon@gmail.com>
2024-02-12 14:29:06 +0100

    Add unit test meson & correctly rust syntax

--- billy_engine/tests/test_engine_exclude_gitaction.rs
+++ billy_engine/tests/test_engine_exclude_gitaction.rs
@@ -1,16 +1,16 @@
 #[cfg(test)]
-mod teste_billy_engine {
-	extern crate billy_engine;
-	use billy_engine::engine::*;
+mod teste_billy_engine_gitaction {
+    extern crate billy_engine;
+    use billy_engine::engine::ScreenData;
 
-	#[test]
-	fn test_screen_data() {
-		const OFFSET: u8 = 1;
-		let mut sd = ScreenData::new();
-		let (_w,h) = sd.size();
-		let h = h - OFFSET as u16;
-		sd.set_offset(OFFSET);
-		assert_eq!(OFFSET, sd.get_offset());
-		assert_eq!(h, sd.get_height());
-	}
+    #[test]
+    fn test_screen_data() {
+        const OFFSET: u8 = 1;
+        let mut sd = ScreenData::new();
+        let h: u16 = sd.get_height() - OFFSET as u16 ;
+        sd.set_offset(OFFSET);
+        assert_eq!(OFFSET, sd.get_offset());
+        assert_eq!(h, sd.get_height());
+    }
 }
+
